f3f28dda22fc3cbc0967e285fc6bf84520cf9cad
Denisa <gijiila_denisa@yahoo.ro>
2020-12-22 16:17:40 +0200

    settings for project

---
+++ src/main/java/santahoho/santahohoapp/SantahohoappApplication.java
@@ -0,0 +1,13 @@
+package santahoho.santahohoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantahohoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantahohoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/santahoho/santahohoapp/SantahohoappApplicationTests.java
@@ -0,0 +1,13 @@
+package santahoho.santahohoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantahohoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
