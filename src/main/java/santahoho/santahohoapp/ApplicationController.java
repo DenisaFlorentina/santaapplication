082e13c4d6998456f0d7ce2431e1179d9a332403
Denisa <gijiila_denisa@yahoo.ro>
2020-12-22 16:35:38 +0200

    -am creat o clasa numita ApplicationController in care am facut o metoda care apeleaza fisierul index din templates

---
+++ src/main/java/santahoho/santahohoapp/ApplicationController.java
@@ -0,0 +1,13 @@
+package santahoho.santahohoapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/index")
+    public String goHome(){
+        return "index";
+    }
+}
